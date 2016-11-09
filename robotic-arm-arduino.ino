29fbdfd812f3084cb21ee2be65942535e50b1fd3
Gabriel Duarte Junior <gabriel181193@hotmail.com>
2016-11-09 01:41:52 -0200

    Update robotic-arm-arduino.ino
    CORREÇÃO DOS ERROS NO SERVOS

--- robotic-arm-arduino.ino
+++ robotic-arm-arduino.ino
@@ -2,9 +2,9 @@
 #include <Servo.h>
 
 #define SERVO_01 6 //Servo base
-#define SERVO_02 5 //Braço principal
-#define SERVO_03 9 //Articulação secundária
-#define SERVO_04 10 //Garra
+#define SERVO_02 10 //Braço principal
+#define SERVO_03 3 //Articulação secundária
+#define SERVO_04 11 //Garra
 
 Servo s_base, s_arm, s_secartic, s_hand;
 
@@ -12,7 +12,7 @@ int pos = 0;
 
 SoftwareSerial BTserial(2, 4);
 
-char *data = "";
+char data = "s";
 
 void setup() 
 {
@@ -24,8 +24,10 @@ void setup()
   Serial.begin(9600);
 
   //Necessário Inicializar a posição inicial de cada servo
-  s_base.write(0);
-  s_arm.write(0);
+  s_base.write(100); // intervalo 0 a 180 
+  s_arm.write(175); // intervalo 125 a 175
+  s_secartic.write(30); // intervalo 0 a 30 
+  s_hand.write(90); // intervalo entre 60 a 90
   
   BTserial.begin(38400);
   
@@ -33,24 +35,9 @@ void setup()
 
 void loop() 
 {
-  //It will be deleted soon
-  if(Serial.available() > 0)
-  {
-  }
-
-  else if(BTserial.available())
-  {
-    data = BTserial.read();
-    for(pos = 0; pos < 180; pos++)
-    {
-      s_base.write(pos);
-      delay(100);
-    }
-    delay(1000);  for(pos = 180; pos >= 0; pos--)
-    {
-      s_base.write(pos);
-      delay(100);
-    }
-    delay(1000);
-  }
+   
+    //data = BTserial.read();
+    if (data=="OC")
+     s_hand.write(90);
+   
 }
